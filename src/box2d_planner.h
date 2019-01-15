a97e86d82a9c9eb3fe141225e0e22720bdaf632d
Akansel Cosgun <akansel.cosgun@monash.edu>
2019-01-15 15:42:48 +1100

    adding box2d library to the project

---
+++ src/box2d_planner.h
@@ -0,0 +1,18 @@
+//System
+#include <iostream>
+#include <stdio.h>
+
+//Box2d
+#include <Box2D/Box2D.h>
+
+class Box2dPlanner {
+  //private:
+
+  
+ public:
+  double asd;
+  Box2dPlanner() {
+    asd = 2.0;
+    std::cout<<"Hello"<<std::endl;
+  }
+};
